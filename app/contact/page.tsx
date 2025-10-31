5ce13f1dc42722cee8df1cb0cd8cef384938a90b
Yuvaraj-0 <yuvaraj525354@gmail.com>
2025-10-31 14:55:42 +0530

    updated all files

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -18,17 +18,14 @@ const  LampDemo = () => {
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         <div className="w-[400] h-[200px] translate-x-[500px] translate-y-[400px]  rounded-xl overflow-hidden shadow-lg">
-  <iframe
-    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d23101.43885822972!2d80.1758486298377!3d13.134533473496843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1761822201912!5m2!1sen!2sin"
-    width="100%"
-    height="100%"
-    allowFullScreen={true}
+        <iframe
+  src="https://www.google.com/maps/embed?..."
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+></iframe>
 
-    style={{ border: 0 }}
-    
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-  ></iframe>
 </div>
       </motion.h1>
 
