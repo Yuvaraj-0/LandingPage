6b3010b2d923041bc21fe75a042c0746e2dfcea5
Yuvaraj-0 <yuvaraj525354@gmail.com>
2025-10-31 14:45:21 +0530

    updated all files

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,7 +22,7 @@ const  LampDemo = () => {
     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d23101.43885822972!2d80.1758486298377!3d13.134533473496843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1761822201912!5m2!1sen!2sin"
     width="100%"
     height="100%"
-    
+    allowFullScreen
     style={{ border: 0 }}
     
     loading="lazy"
